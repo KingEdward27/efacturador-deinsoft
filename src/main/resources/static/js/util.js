41de3e56b9b26f23353a34f336e50c967da5e9a2
EDWARD ROJAS <edward21.sistemas@gmail.com>
2023-01-19 16:10:05 -0500

    fix dialog
    fix dialog

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -4,37 +4,6 @@ $(document).ready(function () {
     $('#loading').hide();
     $.ajaxSetup({cache: false});
 });
-//$(document).ajaxStart(function () {
-//    $("#loading").show();
-//    //$("#loading").show();// show the gif image when ajax starts
-//}).ajaxStop(function () {
-//    $("#loading").hide();
-//});
-$(document).ajaxStart(function () {
-    $(document).ajaxSend(function(evt, request, settings) {
-        ajaxLoader("0");
-     });
-    //$("#loading").show();// show the gif image when ajax starts
-}).ajaxStop(function () {
-    ajaxLoader("hide"); // hide the gif image when ajax completes
-});
-function ajaxLoader(action) {
-    if (action === "hide") {
-        $('body').oLoader(action);
-    } else {
-        $('body').oLoader({
-            image: '../images/loader.gif',
-            wholeWindow: true,
-            lockOverflow: true,
-            effect: 'explode',
-            backgroundColor: '#000',
-            fadeInTime: 1000,
-//            style: "<div style='position:absolute;right:10px;bottom:10px;background:#000;color:#fff;padding:5px;border-radius:4px'>Cargando...</div>",
-            
-            fadeLevel: 0.4
-        });
-    }
-}
 function callConfirmDialog(mensaje, funcion) {
     //    var patron = /\s+/g;//REGX para saber si lo que trae tiene espacios
     //    var val = patron.test(mensaje);
@@ -61,20 +30,4 @@ function callConfirmDialog(mensaje, funcion) {
             }
         }
     });
-}
-function oDatatable() {
-    oTable = $('#dtTable').dataTable({
-        "bProcessing": true,
-        "bJQueryUI": true,
-        "sPaginationType": "full_numbers",
-        "bDestroy": true,
-        "oLanguage": {"sUrl": "../jq/dataTables.spanish.txt"},
-        "aLengthMenu": [
-            [5,25, 50, 100, 200, -1],
-            [5,25, 50, 100, 200, "Todos"]
-        ],
-        "iDisplayLength": 5
-    });
-
-
 }
